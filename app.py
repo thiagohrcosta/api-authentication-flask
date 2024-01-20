d3b48d908d1b9f0d4e68bdf69cf59e23724ccb16
ThiagoHRCosta <thiagohrcosta86@gmail.com>
2024-01-20 18:12:34 -0300

    Update and delete user

--- app.py
+++ app.py
@@ -54,6 +54,47 @@ def create_user():
 
   return jsonify({'message': 'Invalid data'}), 400
 
+@app.route('/user/<int:id_user>', methods=['GET'])
+@login_required
+def read_user(id_user):
+  user = User.query.get(id_user)
+
+  if user:
+    return {
+      "username": user.username
+    }
+  
+  return jsonify({"message": "User not found."}), 404
+
+@app.route('/user/<int:id_user>', methods=['PUT'])
+@login_required
+def update_user(id_user):
+  data = request.json
+  user = User.query.get(id_user)
+
+  if user and data.get("password"):
+    user.password = data.get("password")
+    db.session.commit()
+
+    return jsonify({"message": "User successfully updated."})
+  
+  return jsonify({"message": "User not found."}), 404
+
+@app.route('/user/<int:id_user>', methods=['DELETE'])
+@login_required
+def delete_user(id_user):
+  user = User.query.get(id_user)
+
+  if id_user == current_user.id:
+    return jsonify({"message": "Delete method not allowed."}), 403
+  
+  if user: 
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({"message": f"User {id_user} successfully deleted."})
+  
+  return jsonify({"message": "User not found."}), 404
+
 @app.route('/hello-world', methods=['GET'])
 def hellow_world():
   return 'Hello world'
