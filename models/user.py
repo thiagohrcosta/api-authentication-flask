bd1913ceb07abbb1136b57025a0c035416259554
ThiagoHRCosta <thiagohrcosta86@gmail.com>
2024-01-24 00:17:27 -0300

    Adding User role and bcrypt for passwords

--- models/user.py
+++ models/user.py
@@ -5,4 +5,5 @@ class User(db.Model, UserMixin):
   # id (int), username (text), password(text)
   id = db.Column(db.Integer, unique=True, primary_key=True)
   username = db.Column(db.String(32), unique=True, nullable=False)
-  password = db.Column(db.String(8), nullable=False)
+  password = db.Column(db.String(80), nullable=False)
+  role = db.Column(db.String(8), nullable=False, default='user')
